61add75f5ee3c4fcc821301bcbd85f38f304ff71
Areasus <cantosh.s@gmail.com>
2022-11-10 22:29:31 +0545

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 const API_KEY = config.API_KEY
+const DAYS= ["sun","mon","tues","wed","thu","fri","sat"]
 const getCurrentWeatherData = async(city)=>{
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_KEY+"&units=metric")
     return response.json()
 }
+
 
 const getHourlyData =async (city)=>{
     const response = await fetch(`https:/api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`)
@@ -15,7 +17,7 @@ const getHourlyData =async (city)=>{
 }
 
 const formatTemp =(temp)=>{
-    return `${temp?.toFixed(1)}°`
+    return `${temp?.toFixed(1)}°C`
 }
 
 const createUrl= (icon)=> `http://openweathermap.org/img/wn/${icon}@2x.png`
@@ -57,14 +59,66 @@ const pushHumidity=({main:{humidity}})=>{
 
 }
 
+const calculateForecast = (info)=>{
+    let daywiseFc =new Map()
+    for (let i of info){
+        const [date] =i.dt_txt.split(" ")
+        const dayOfWeek = DAYS[new Date(date).getDay()]
+        if (daywiseFc.has(dayOfWeek)){
+            let fcDay=daywiseFc.get(dayOfWeek)
+            fcDay.push(i)
+            daywiseFc.set(dayOfWeek,fcDay)
+        }
+        else{
+            daywiseFc.set(dayOfWeek,[i])
+        }
+    }
+    for (let [key,value] of daywiseFc)
+    {
+        let minTemp= Math.min(...Array.from(value,val=>val.temp_min))
+        let maxTemp= Math.max(...Array.from(value,val=>val.temp_max))
+        daywiseFc.set(key,{temp_min:minTemp,temp_max:maxTemp,icon:value.find(v=>v.icon).icon})
+    }
+    return daywiseFc
+}
+
+const pushFiveDay = (info)=>{
+    console.log(info)
+    const daywise = calculateForecast(info)
+    const container = document.querySelector(".five-days")
+    let innerHTML=``
+    Array.from(daywise).map(([day,{temp_max,temp_min,icon}],index)=>{
+       
+        innerHTML+=`<article class="days">
+        <h2>${index===0? "Today":day}</h2>
+        <img src="${createUrl(icon)}" alt="${icon}">
+        <p class="five-min-temp">${formatTemp(temp_min)}</p>
+        <p class="five-max-temp">${formatTemp(temp_max)}</p>
+        </article>`
+
+    })
+    container.innerHTML =innerHTML
+}
+
 
 document.addEventListener("DOMContentLoaded",async()=>{
-    // console.log(await getCurrentWeatherData("pokhara"))
     const info = await getCurrentWeatherData("pokhara")
     pushCurrentData(info)
     const hourly = await getHourlyData("pokhara")
     pushHourlyData(hourly)
     pushFeelsLike(info)
     pushHumidity(info)
-
+    pushFiveDay(hourly)
+    const dropdown = document.querySelector("#cities");
+    dropdown.addEventListener("change",async(event)=>{
+        const ct=event.target.value
+        const info = await getCurrentWeatherData(ct)
+        pushCurrentData(info)
+        const hourly = await getHourlyData(ct)
+        pushHourlyData(hourly)
+        pushFeelsLike(info)
+        pushHumidity(info)
+        pushFiveDay(hourly)
+    })
 })
+
